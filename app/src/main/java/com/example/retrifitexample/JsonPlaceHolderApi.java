e078ce459eb1f6c7963097d80d7fa95827569add
Haider <shkhaider2015@gmail.com>
2020-06-18 03:12:41 +0500

    zz

--- app/src/main/java/com/example/retrifitexample/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/retrifitexample/JsonPlaceHolderApi.java
@@ -1,19 +1,48 @@
 package com.example.retrifitexample;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonPlaceHolderApi {
 
-    @GET("posts")
-    Call<List<Post>> getPosts();
+    // First Example
+//    @GET("posts")
+//    Call<List<Post>> getPosts();
 
+    // Query url
+//    @GET("posts")
+//    Call<List<Post>> getPosts(@Query("userId") int userId);
+
+    // Complex Query
+    @GET("posts")
+    Call<List<Post>> getPosts(
+            @Query("userId") int userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    //Query Map example
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
 //    @GET("posts/2/comments")
 //    Call<List<Comment>> getComments();
 
+    // dynamic changes
     @GET("posts/{id}/comments")
     Call<List<Comment>> getComments(@Path("id") int postid);
+
+    @GET
+    Call<List<Comment>> getComments(@Url String url);
+
+    @POST("posts")
+    Call<Post> createPost(@Body Post post);
 }
--- app/src/main/java/com/example/retrifitexample/MainActivity.java
+++ app/src/main/java/com/example/retrifitexample/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -35,13 +37,26 @@ public class MainActivity extends AppCompatActivity {
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
 //        getPosts();
-          getComments();
+//          getComments();
+        createPost();
     }
 
     private void getPosts()
     {
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        int userId = 4;
+//        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(userId);
+        //Complex Query
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(userId, "id", "desc");
 
+        //in case we dont use params
+//        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(userId, null, null);
+
+        // For Query Map
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("userId", "1");
+        parameters.put("_sort", "id");
+        parameters.put("_order", "desc");
+        Call<List<Post>> callMap = jsonPlaceHolderApi.getPosts(parameters);
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
@@ -72,7 +87,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void getComments()
     {
-        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(3);
+        int postId = 3;
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(postId);
+
+        Call<List<Comment>> callURL = jsonPlaceHolderApi.getComments("posts/2/comments");
 
         call.enqueue(new Callback<List<Comment>>() {
             @Override
@@ -102,5 +120,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void createPost()
+    {
+        Post post = new Post(33, "New Title", "New Message Text");
+
+        Call<Post> call = jsonPlaceHolderApi.createPost(post);
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response)
+            {
+                if (!response.isSuccessful())
+                {
+                    textViewResult.setText("Code : " + response.code());
+                    return;
+                }
+                Post postResponse = response.body();
+
+                    String content = "";
+                    content += "Code : " + response.code() +
+                            "\nID : " + postResponse.getId() +
+                            "\n Title : " + postResponse.getTitle() +
+                            "\n Body : " + postResponse.getText() +
+                            "\n\n";
+                    textViewResult.append(content);
+
+
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+
+            }
+        });
     }
 }
